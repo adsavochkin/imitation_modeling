73737fb8cbeefdecc7451ac3248a4c0bd55fcd33
adsavochkin <167904812+adsavochkin@users.noreply.github.com>
2024-04-23 17:43:24 +0000

    Main

--- VS_Job/FLOW.py
+++ VS_Job/FLOW.py
@@ -1,13 +1,13 @@
-# import numpy as np
-# import pandas as pd
+import numpy as np
+import pandas as pd
 
 import DATA_LOAD as dl
 import INITIALIZATION as ini
 import JOB_SHOP as js
 
-# file = 'competition_case.xlsx'
-# file = 'test_case.xlsx'
-file = 'test_case_new.xlsx'
+file = 'competition_case.xlsx'
+#file = 'test_case.xlsx'
+#file = 'test_case_new.xlsx'
 
 #   Загрузка данных из файла
 with dl.load_from_file(file) as db:
@@ -19,18 +19,17 @@ with dl.load_from_file(file) as db:
     movement_time = db.load_movement_time() # Время на перемещение между оборудованием
     
     orders = db.load_orders() # Цены и финальные продукты
-
 #   Предварительная обработка данных
-with ini.preprocessing_data(subproduct,structure,equipment,switch_time,order_graph,movement_time) as preData:
-    interval_data = preData.pre_interval_data() # Комбинированные данные для интервальной переменной
-    imply_data = preData.imply_data() # Данные для последовательности выполнения операций
-    start_stop_data = preData.start_stop_data() # Интервалы требующие время на перемещение между оборудованием
+    with ini.preprocessing_data(subproduct,structure,equipment,switch_time,order_graph,movement_time) as preData:
+        interval_data = preData.pre_interval_data() # Комбинированные данные для интервальной переменной
+        imply_data = preData.imply_data() # Данные для последовательности выполнения операций
+        start_stop_data = preData.start_stop_data() # Интервалы требующие время на перемещение между оборудованием
 
 
-#   Расчет модели
-problem = js.job_shop_model(orders,interval_data,imply_data,start_stop_data,file)
-problem.solve()
+    #   Расчет модели
+    problem = js.job_shop_model(orders,interval_data,imply_data,start_stop_data,file)
+    problem.solve()
 
-#   Выгрузка результатов
-solution = problem.output_data()
-solution.to_csv('solution.csv', sep='\t', encoding='utf-8')
+    #   Выгрузка результатов
+    solution = problem.output_data()
+    solution.to_csv('solution.csv', sep='\t', encoding='utf-8')
